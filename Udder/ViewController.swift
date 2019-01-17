c79865e9d86da210757d6545b35df86cd2be5a63
Ryan <ryanhuebert@gmail.com>
2019-01-16 16:28:59 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ryan Huebert on 1/16/19.
+//  Copyright © 2019 Ryan Huebert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
